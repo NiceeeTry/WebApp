a65b97d1d22bc1ef065a4db6f71dcd714fb81b9f
Alikhan <alikhanaitbayevpost@gmail.com>
2023-06-18 20:15:14 +0600

    view handler

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from panic import vovels
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ def do_search()->str:
     letters = request.form['letters']
     title = 'Here are your results:'
     results = vovels(phrase,letters)
+    log_request(request,results)
     return render_template('results.html',the_phrase=phrase,
                            the_letters = letters,
                            the_title=title,
@@ -17,4 +18,23 @@ def do_search()->str:
 @app.route('/entry')
 def entry_page()->'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
-app.run(debug=True)
+
+
+def log_request(req:'flask_request', res:str)->None:
+    with open('vsearch.log','a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+        
+
+@app.route('/viewlog')
+def view()->'str':
+    contents = []
+    with open('vsearch.log') as f:
+        for line in f:
+            contents.append([])
+            for item in line.split("|"):
+                contents[-1].append(escape(item))
+    return str(contents)
+            
+
+if __name__=="__main__":
+    app.run(debug=True)
